8e003ebbcd2a31e593fe6b04b76cd5e6a918cd61
BeYoddy <changwoon4@naver.com>
2023-03-22 21:26:22 +0900

    수치연산자축약

---
+++ java100.java
@@ -0,0 +1,17 @@
+public class java100 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        a = 60;
+        b = 8;
+        c = 300;
+        d = 400;
+
+        System.out.println(a + b);
+        System.out.println("a+b=" + a + b);
+        System.out.println("a+b=" + (a + b));
+        System.out.println("a-b=" + (a - b));
+        System.out.println("a*b=" + (a * b));
+        System.out.println("a/b=" + (a / b));
+        System.out.println("a%b=" + (a % b));
+    }
+}
---
+++ operator.java
@@ -0,0 +1,20 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 0, b = 100;
+        a = a + 1;
+        System.out.println(a);
+
+        a += 1;
+        System.out.println(a);
+
+        a -= 1;
+        System.out.println(a);
+
+        b++;
+        System.out.println(b);
+        b++;
+        System.out.println(b);
+        b--;
+        System.out.println(b);
+    }
+}
